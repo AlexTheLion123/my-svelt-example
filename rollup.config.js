3189a3c977a57ee4e1bf20e0772859768fa3df9c
Alex <alexurban12@gmail.com>
2021-09-17 16:17:04 +0200

    basic starter template

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
+// whenever rollup runs in watch mode, it automatically sets this environment variable to 'development'
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+    input: "src/main.js",
+    output: {
+        format: 'iife',
+        file: 'public/build/bundle.js'
+    },
+    plugins: [
+        svelte({
+            include: 'src/*.svelte'
+        }),
+        resolve({
+            browser: true
+        }),
+        !production && livereload({
+            watch: 'public'
+        }),
+        !production && serve({
+            open: true,
+            openPage: '/index.html',
+            contentBase: './public',
+            host: 'localhost',
+            port: 10001,
+        })
+    ]
+}
